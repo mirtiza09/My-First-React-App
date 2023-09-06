deb6c65ca86ffad7274fac89a529a3e56ba25b98
Muhammad Irtiza <m.irtiza09@gmail.com>
2023-09-06 10:07:35 +1000

    Static React App Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import List from './components/List';
 import './App.css';
 
+const tasks = [
+    {description: 'Learn Beact', completed:true},
+    {description: 'Learn JSX', completed:false},
+    {description: 'Build a React App', completed:false}
+];
+
+
 function App() {
     return (
         <div className='page'>     
-            <List heading='My Tasks'/>
+            <List heading='My Tasks' tasks={tasks}/>
         </div>
     );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,39 @@
+function Task(props) {
+	return (
+		<li>{props.description}<input type="checkbox" checked={props.completed} readOnly/></li>
+		);
+	
+}
+
+
+
 function List(props) {
+	var taskArray = [];
+	for (let i=0; i<props.tasks.length;i++)
+	{
+		let desc = props.tasks[i].description;
+		let stat = props.tasks[i].completed;
+		taskArray.push(<Task description = {desc} completed = {stat}/>)
+	}
+	
 	return (
 		<div>
-			<h1>{ props.heading }</h1>
+			<h1>{props.heading}</h1>
 			<ul>
-				<li>Learn React</li>
-				<li>Learn JSX</li>
-				<li>Build a React App</li>
+				{props.tasks.map(task =>
+					<Task
+					description = {task.description}
+					completed = {task.completed}
+					/>
+				)}
 			</ul>
 		</div>
 	);
 }
 
 export default List;
+
+
+				/*<Task description = {props.tasks[0].description} completed={props.tasks[0].completed}/>
+				<Task description = {props.tasks[1].description} completed={props.tasks[1].completed}/>
+				<Task description = {props.tasks[2].description} completed={props.tasks[2].completed}/> */
